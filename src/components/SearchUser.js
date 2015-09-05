ef8705d3db218b6e03c7eeb3e3b8c178a89770db
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2015-09-05 01:15:04 -0400

    Routing works to search users

--- src/components/SearchUser.js
+++ src/components/SearchUser.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 
-export default class SearchUser extends Component {
+class SearchUser extends Component {
+  searchUser() {
+    const { router } = this.context;
+    const { value:userName } = this.refs.userName.getDOMNode();
+
+    router.transitionTo(userName, {});
+  }
+
   render() {
     return (
       <div className="input-group col-sm-12">
         <div className="form-group col-sm-7">
-          <input type="text" className="form-control" placeholder="Search User..." />
+          <input type="text" className="form-control" placeholder="Search User..." ref="userName" />
         </div>
         <div className="form-group col-sm-5">
-          <button className="btn btn-block btn-primary">Search</button>
+          <button className="btn btn-block btn-primary" onClick={this.searchUser.bind(this)}>Search</button>
         </div>
       </div>
     );
   }
 }
+
+SearchUser.contextTypes = {
+  router: React.PropTypes.object.isRequired
+}
+
+export default SearchUser;
