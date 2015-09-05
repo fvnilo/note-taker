6a60a8e433a63fd61f17407e84bf9a92ff01d0af
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2015-09-05 11:00:43 -0400

    Display user profile

--- src/components/github/User.js
+++ src/components/github/User.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 
 export default class User extends Component {
   render() {
-    return (<h1>User</h1>);
+    const { user } = this.props;
+
+    return (
+      <div>
+        <h2>User Profile</h2>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <img src={user.avatarUrl} className="img-responsive"/>
+          </li>
+          { user.name && <li className="list-group-item">Name: {user.name}</li> }
+          { user.email && <li className="list-group-item">Email: {user.email}</li> }
+          { user.blog && <li className="list-group-item">Blog: <a href={user.blog}>{user.blog}</a></li> }
+          { user.company && <li className="list-group-item">Company: {user.company}</li> }
+          { user.location && <li className="list-group-item">Location: {user.location}</li>}
+          <li className="list-group-item">Hireable: {user.hireable && 'Yes'} {!user.hireable && 'No'}</li>
+        </ul>
+      </div>);
   }
 }
--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -3,19 +3,44 @@ import { createReducer } from './helpers';
 import { REQUEST_PROFILE, RECEIVE_PROFILE } from '../constants/ActionTypes.js';
 
 const initialProfileState = {
-  bio: {},
+  user: {},
   repos: [],
   isFetching: false
 };
 
+function buildBio(profile) {
+  const {
+    name,
+    location,
+    email,
+    followers,
+    following,
+    company,
+    blog,
+    hireable,
+    avatar_url: avatarUrl} = profile;
+
+  return {
+    name,
+    location,
+    email,
+    avatarUrl,
+    followers,
+    following,
+    company,
+    blog,
+    hireable
+  };
+}
+
 export const profile = createReducer(initialProfileState, {
-  [REQUEST_PROFILE]: (state, action) => {
+  [REQUEST_PROFILE]: (state) => {
     return Object.assign({}, state, {
       isFetching: true
     });
   },
   [RECEIVE_PROFILE]: (state, action) => {
-    return Object.assign({}, state, {
+    return Object.assign({}, { user: buildBio(action.profile) }, {
       isFetching: false
     });
   }
