2251f00769d08277aae39240e09b78a62352984b
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2015-09-20 11:40:50 -0400

    Handle the enter key in the search user component and the add note component

--- src/components/SearchUser.js
+++ src/components/SearchUser.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
+import { ENTER_KEY_CODE } from '../constants/Constants';
+
 class SearchUser extends Component {
+  onKeyDown(e) {
+    if (e.keyCode === ENTER_KEY_CODE) {
+      this.searchUser();
+    }
+  }
+
   searchUser() {
     const { onSearchUser } = this.props;
     const { value: userName } = this.refs.userName.getDOMNode();
@@ -12,7 +20,7 @@ class SearchUser extends Component {
     return (
       <div className="input-group col-sm-12">
         <div className="form-group col-sm-7">
-          <input type="text" className="form-control" placeholder="Search User..." ref="userName" />
+          <input type="text" className="form-control" placeholder="Search User..." onKeyDown={this.onKeyDown.bind(this)} ref="userName" />
         </div>
         <div className="form-group col-sm-5">
           <button className="btn btn-block btn-primary" onClick={this.searchUser.bind(this)}>Search</button>
--- src/components/notes/AddNote.js
+++ src/components/notes/AddNote.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import { ENTER_KEY_CODE } from '../../constants/Constants';
 
 class AddNote extends Component {
+  onKeyDown(e) {
+    if (e.keyCode === ENTER_KEY_CODE) {
+      this.onAddNote();
+    }
+  }
+
   onAddNote() {
     const { addNote } = this.props;
     const { value: note } = this.refs.note.getDOMNode();
 
-    addNote(note);
-
+    if (note) {
+      addNote(note);
+    }
+    
     this.refs.note.getDOMNode().value = '';
   }
 
   render() {
     return (
       <div className="input-group">
-        <input type="text" className="form-control" ref="note" placeholder="Add New Note..." />
+        <input type="text" className="form-control" ref="note" placeholder="Add New Note..." onKeyDown={this.onKeyDown.bind(this)} />
         <span className="input-group-btn">
           <button className="btn btn-default" onClick={this.onAddNote.bind(this)}>Add</button>
         </span>
